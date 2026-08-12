5b1f7dc2d92a160f6a0fc6ddf228ea45def194b9
volleyavuz <volleyavuz@gmail.com>
2026-08-12 23:18:20 +0300

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.parion.aidat"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.parion.aidat"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1-pilot"
+    }
+}
