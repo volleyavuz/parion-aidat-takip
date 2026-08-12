d0ece120aaba04740791e9f9b050b7024b2c8470
volleyavuz <volleyavuz@gmail.com>
2026-08-13 01:38:59 +0300

    Resolve Kotlin stdlib conflict for v2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,12 @@ android {
     }
 }
 
+configurations.all {
+    resolutionStrategy.force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+}
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
 }
