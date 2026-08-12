da3a1ba76b8b1899dc4331f97ec101a89c84b27b
volleyavuz <volleyavuz@gmail.com>
2026-08-13 02:21:06 +0300

    Bump pilot to v2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,6 @@
 plugins { id("com.android.application") }
-
-android {
-    namespace = "com.parion.aidat"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "com.parion.aidat"
-        minSdk = 23
-        targetSdk = 36
-        versionCode = 3
-        versionName = "2.1-pilot"
-    }
+android { namespace="com.parion.aidat"; compileSdk=36
+ defaultConfig { applicationId="com.parion.aidat"; minSdk=23; targetSdk=36; versionCode=4; versionName="2.2-pilot" }
 }
-
-configurations.all {
-    resolutionStrategy.force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.1")
-}
+configurations.all { resolutionStrategy.force("org.jetbrains.kotlin:kotlin-stdlib:1.8.22"); exclude(group="org.jetbrains.kotlin",module="kotlin-stdlib-jdk7"); exclude(group="org.jetbrains.kotlin",module="kotlin-stdlib-jdk8") }
+dependencies { implementation("androidx.appcompat:appcompat:1.7.1") }
