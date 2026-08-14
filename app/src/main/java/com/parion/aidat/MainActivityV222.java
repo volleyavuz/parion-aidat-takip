b5ba4ccc69582db2e3e336e9b0d45fc02956bb60
volleyavuz <volleyavuz@gmail.com>
2026-08-14 21:31:29 +0300

    Remove obsolete v2.2 activity from v3 pilot

--- app/src/main/java/com/parion/aidat/MainActivityV222.java
+++
@@ -1,8 +0,0 @@
-package com.parion.aidat;
-
-public class MainActivityV222 extends MainActivity {
-    @Override
-    String tl(double x) {
-        return String.format(TR, "%,d ₺", java.lang.Math.round(x));
-    }
-}
