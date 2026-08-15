0cbce0363f2e1a2d0a45606e162f02dee1f984cc
volleyavuz <volleyavuz@gmail.com>
2026-08-15 03:56:44 +0300

    Provide lightweight zoom image base without external dependency

---
+++ app/src/main/java/androidx/appcompat/widget/AppCompatImageView.java
@@ -0,0 +1,5 @@
+package androidx.appcompat.widget;
+
+public class AppCompatImageView extends android.widget.ImageView {
+    public AppCompatImageView(android.content.Context context) { super(context); }
+}
