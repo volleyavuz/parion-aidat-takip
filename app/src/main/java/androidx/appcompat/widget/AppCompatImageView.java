31b128d4567a5d4761d39e4ed76b8fee4e260427
volleyavuz <volleyavuz@gmail.com>
2026-08-16 00:33:41 +0300

    Support zoom viewer drawable wrapper

--- app/src/main/java/androidx/appcompat/widget/AppCompatImageView.java
+++ app/src/main/java/androidx/appcompat/widget/AppCompatImageView.java
@@ -2,4 +2,8 @@ package androidx.appcompat.widget;
 
 public class AppCompatImageView extends android.widget.ImageView {
     public AppCompatImageView(android.content.Context context) { super(context); }
+    @Override public com.parion.aidat.Drawable getDrawable(){
+        android.graphics.drawable.Drawable d=super.getDrawable();
+        return d==null?null:new com.parion.aidat.Drawable(d);
+    }
 }
