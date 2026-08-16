467e8e79d4b631e466918ae36ee0e60cdb1803ca
volleyavuz <volleyavuz@gmail.com>
2026-08-17 02:35:10 +0300

    Bump media hotfix to v3.8.33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 50
-        versionName = "3.8.32"
+        versionCode = 51
+        versionName = "3.8.33"
     }
 
     signingConfigs {
