052abb52c07def055557ddb5d02ce3a2eeabe4eb
volleyavuz <volleyavuz@gmail.com>
2026-08-17 15:15:07 +0300

    Bump version to 3.8.42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 59
-        versionName = "3.8.41"
+        versionCode = 60
+        versionName = "3.8.42"
     }
 
     signingConfigs {
