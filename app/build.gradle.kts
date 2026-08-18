1170862582a56076e2e1d4969eb11cc60a82dbad
volleyavuz <volleyavuz@gmail.com>
2026-08-19 01:12:52 +0300

    Bump to v3.8.61 ANR scheduler hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 78
-        versionName = "3.8.60"
+        versionCode = 79
+        versionName = "3.8.61"
     }
 
     signingConfigs {
