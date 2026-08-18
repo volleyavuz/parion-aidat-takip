ac9e434579fc6917bf3aea72b750978735fe8ff3
volleyavuz <volleyavuz@gmail.com>
2026-08-18 03:49:05 +0300

    Bump hotfix to v3.8.55

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 72
-        versionName = "3.8.54"
+        versionCode = 73
+        versionName = "3.8.55"
     }
 
     signingConfigs {
