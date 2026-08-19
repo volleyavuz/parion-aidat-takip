db19b7e2ebb0677e3980b1a4dc5275e11a310561
volleyavuz <volleyavuz@gmail.com>
2026-08-20 02:45:06 +0300

    Bump Parion to v4.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 107
-        versionName = "4.0.7"
+        versionCode = 108
+        versionName = "4.0.8"
     }
 
     signingConfigs {
