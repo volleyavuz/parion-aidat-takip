d94807f94af86bc26892af6550c83ac05518ef28
volleyavuz <volleyavuz@gmail.com>
2026-08-24 01:23:10 +0300

    Bump version to 4.0.43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 142
-        versionName = "4.0.42"
+        versionCode = 143
+        versionName = "4.0.43"
     }
 
     signingConfigs {
