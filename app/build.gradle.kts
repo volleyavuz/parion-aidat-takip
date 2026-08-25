9a508ba7bed795472d6d6e25227f988bd6e8a43c
volleyavuz <volleyavuz@gmail.com>
2026-08-25 16:54:09 +0300

    Bump version to 4.0.94

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,9 +8,9 @@ android {
         applicationId = "com.parion.aidat"
         minSdk = 23
         targetSdk = 36
-        versionCode = 194
-        versionName = "4.0.92-R1"
-        // Functional baseline: v4.0.92
+        versionCode = 195
+        versionName = "4.0.94"
+        // Functional baseline: v4.0.92-R1
         // Recovery baseline: v4.0.70
     }
 
